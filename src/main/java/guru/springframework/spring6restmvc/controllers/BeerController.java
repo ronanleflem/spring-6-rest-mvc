b04bd1edb15ee0e6219914d03ed22bb0d66b1f32
ronanleflem <ronan.leflem@gmail.com>
2024-08-12 21:20:23 +0200

    Use Path Parameters

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -5,7 +5,9 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,16 +16,18 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
-    @RequestMapping("api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeer(){
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID id) {
         log.debug("Get beer by id - Controller");
 
         return beerService.getBeerById(id);
