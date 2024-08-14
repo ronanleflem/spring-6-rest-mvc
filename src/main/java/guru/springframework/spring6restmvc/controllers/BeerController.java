5f3d8c4c267d447a1d4639f1cc83b0f654261def
ronanleflem <ronan.leflem@gmail.com>
2024-08-14 15:10:13 +0200

    Controller Advice : gérer globalement les exceptions dans l'application

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -26,12 +26,6 @@ public class BeerController {
 
     private final BeerService beerService;
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity handleNotFoundException(){
-        System.out.println("In exception ");
-        return ResponseEntity.notFound().build();
-    }
-
     @PatchMapping(BEER_PATH_ID)
     public ResponseEntity updateBeerPatchById(@PathVariable("beerId")UUID beerId, @RequestBody Beer beer){
 
