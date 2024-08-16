376547c533a896360359f24ed67edfbc6e5dec14
ronanleflem <ronan.leflem@gmail.com>
2024-08-16 13:18:16 +0200

    JPA Get Operations, Controller Integration Test and Testiong expected Exceptions (Customer)

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.mappers.BeerMapper;
+import guru.springframework.spring6restmvc.mappers.CustomerMapper;
 import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -10,23 +12,28 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
 @RequiredArgsConstructor
 public class CustomerServiceJPA implements CustomerService {
 
-    private final BeerRepository beerRepository;
-    private final BeerMapper beerMapper;
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
 
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID uuid) {
-        return Optional.empty();
+        return Optional.ofNullable(customerMapper.customerToCustomerDto(customerRepository.findById(uuid)
+                .orElse(null)));
     }
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return List.of();
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
     }
 
     @Override
