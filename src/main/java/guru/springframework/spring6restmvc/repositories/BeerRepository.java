0a409a2e0d2aa02c1b6f1c1063f93c26e94d5ae5
ronanleflem <ronan.leflem@gmail.com>
2024-09-16 17:39:19 +0200

    Find By Name with Spring Data JPA

--- src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BeerRepository extends JpaRepository<Beer, UUID>{
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 
 }
